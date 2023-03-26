da33d245cc621a3cfd76616bbaff511fef6f5d84
Арсений Изимов <arsenyxiii@gmail.com>
2023-03-26 23:41:45 +0300

    add command load db ingredients

---
+++ backend/foodgram/food/management/commands/command.py
@@ -0,0 +1,32 @@
+import json
+import sys
+
+from django.core.management.base import BaseCommand
+
+from food.models import Ingredient
+
+
+class Command(BaseCommand):
+    """
+    Команда для извлеения данных из csv.
+    Запуск python manage.py command имя сsv файла.
+    """
+    help = 'Создает Данные из csv, параметр название файла'
+
+    def add_arguments(self, parser):
+        parser.add_argument('file', type=str, help='Путь к файлу')
+
+    def handle(self, *args, **kwargs):
+        file_name = kwargs['file']
+        file = f'data/{file_name}.json'
+        try:
+            with open(file, "r", encoding="utf-8-sig") as json_file:
+                data = json.load(json_file)
+                for row in data:
+                    print(row)
+                    Ingredient.objects.update_or_create(
+                        **row
+                    )
+        except IOError:
+            print("Нет файла!")
+            sys.exit()
